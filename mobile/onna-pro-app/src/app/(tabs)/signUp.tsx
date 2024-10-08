fcdb63bdf11e982b8f1e4830a8f71ef95923bf98
Caio Henrique Dumont Ferreira <caiodumontfer@gmail.com>
2024-10-08 10:10:16 -0300

    add img

--- mobile/onna-pro-app/src/app/(tabs)/signUp.tsx
+++ mobile/onna-pro-app/src/app/(tabs)/signUp.tsx
@@ -8,14 +8,14 @@ export default function SignUp() {
 
                 <View className="flex-row items-center justify-center" >
                     <Image className="w-[225px] h-[242px] m-[5px] mb-[10px]"
-                        source={require("@/assets/images/woman.png")}
+                        source={require("@/assets/images/reporting.png")}
                     />    
                 </View>
 
                 <KeyboardAvoidingView className="flex-1" >
                     <ScrollView contentContainerStyle={{ flexGrow: 1 }} >
                         <View className="w-full h-full bg-white items-center justify-center gap-[5px]" >
-                            <Text className="w-[75%] text-[27px] text-left color-black font-Ibold mb-[3px]" >Cadastro</Text>
+                            <Text className="w-[75%] text-[27px] text-left color-green-800 font-Ibold mb-[3px]" >Cadastro</Text>
                             <Text className="w-[75%] text-[18px] text-left color-black font-Omedium mt-[10px] mb-[3px]" >Nome</Text>
                             <TextInput className="w-[75%] h-[45px] bg-white border-solid border-green-800 border-[1px] color-black text-[18px] text-justify font-Oregular rounded-[10px] mb-[6px] pl-[10px]"
                                 placeholder="Name"
